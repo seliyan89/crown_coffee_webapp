74ba0e77b890563b7cb109191d3d51043640044c
Seliyan Silvarajoo <seliyansilvarajoo@Seliyans-MacBook-Pro.local>
2018-04-20 13:47:14 +0800

    sign-up V2

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -1,4 +1,13 @@
 class MyRegistrationsController < Devise::RegistrationsController
+	
+  def destroy
+    resource.soft_delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(reource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
+
 	private
 
 	def sign_up_params
@@ -6,6 +15,6 @@ class MyRegistrationsController < Devise::RegistrationsController
 	end
 
 	def account_update_params
-		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+		params.require(:user).permit(:first_name, :last_name, :email, :phone_number_mobile, :phone_number_office, :password, :password_confirmation, :current_password)
 	end
 end
