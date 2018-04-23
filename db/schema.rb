d454630b692c3d913f49c879bc7e957703442e74
ElaineChoo <elainechoo85@gmail.com>
2018-04-23 19:27:04 +0800

    new and edit forms done

--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,6 @@ ActiveRecord::Schema.define(version: 2018_04_23_051203) do
     t.string "img"
     t.boolean "is_deleted"
     t.boolean "is_available"
-    t.string "sku"
-    t.string "parent"
-    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
