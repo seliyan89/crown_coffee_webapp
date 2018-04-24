4f9ea11257d830a2fdedd3ec287e15727062c524
jchuajy <j.chua.jy@gmail.com>
2018-04-24 15:09:42 +0800

    half complete variations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_051203) do
+ActiveRecord::Schema.define(version: 2018_04_24_065829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -26,8 +26,10 @@ ActiveRecord::Schema.define(version: 2018_04_23_051203) do
     t.boolean "is_complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "variation_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+    t.index ["variation_id"], name: "index_orders_on_variation_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -42,6 +44,15 @@ ActiveRecord::Schema.define(version: 2018_04_23_051203) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products_variations", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "variation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_products_variations_on_product_id"
+    t.index ["variation_id"], name: "index_products_variations_on_variation_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,6 +78,21 @@ ActiveRecord::Schema.define(version: 2018_04_23_051203) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "variations", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "sku"
+    t.float "price"
+    t.string "category"
+    t.boolean "is_deleted"
+    t.boolean "is_available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "orders", "variations"
+  add_foreign_key "products_variations", "products"
+  add_foreign_key "products_variations", "variations"
 end
